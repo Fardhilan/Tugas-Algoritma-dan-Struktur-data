090d00b644094e458106aa3ebc5f95d0a2d7f95e
김기명 <93815689+Fardhilan@users.noreply.github.com>
2022-10-06 00:28:01 +0700

    Add files via upload

---
+++ Tugas Algoritma&Struktur Data.py
@@ -0,0 +1,16 @@
+Pesan='Mengapa kamu begitu menarik'
+print("Teringat:")
+print(Pesan)
+
+Pesan2="Namun semua tak semudah itu"
+print("\nApa mungkin Tuhan punya rencana:")
+print(Pesan2)
+
+Pesan3='''Yang terindah belum tentu yang terbaik"Semoga ditakdirkan bersama"'''
+print("\nCinta kita:")
+print(Pesan3)
+
+Pesan4='''Haruskah 
+Aku mencintaimu'''
+print("\nSemoga ditakdirkan bersama:")
+print(Pesan4)
